20182eb901aadfccc04e6e08c4c4ad8f894775e9
QuispeLuis <luisqtic@gmail.com>
2022-04-16 18:42:58 -0300

    primer commit

---
+++ ProjectPracticar/src/main/java/asdasd/Principal.java
@@ -0,0 +1,14 @@
+package asdasd;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numero=320;
+		do {
+			System.out.println(numero);
+			numero = numero - 20;
+		}while (numero>=160);
+	}
+
+}
